2dbc40b93a37ceafc9f9ad98a36118832e927e26
FrancescoBottaro <117718788+FrancescoBottaro@users.noreply.github.com>
2022-12-13 23:59:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 char nome [10] [20];
-char test_nome  [20] = "francesco";
+char test_nome  [20];
 
 int main() {
 
     for (int i = 0; i < 10; i++) {
     cin >> nome[i];
 }
-
+    cin>>test_nome;
     ris (nome,test_nome );
 
     return 0;
