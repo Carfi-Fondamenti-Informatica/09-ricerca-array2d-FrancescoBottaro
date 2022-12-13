3984a76b6fb67dd7208a21e863e2201214ac40fa
FrancescoBottaro <117718788+FrancescoBottaro@users.noreply.github.com>
2022-12-13 22:07:22 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include "lib.h"
 
-int main(){
-  return 0;
+
+using namespace std;
+char nome [10] [20];
+char test_nome [1] [20];
+
+int main() {
+
+    for (int i = 0; i < 10; i++) {
+    cin >> nome[i];
+}
+
+         cin >> test_nome[0];
+         ris (nome,test_nome);
+
+    return 0;
 }
