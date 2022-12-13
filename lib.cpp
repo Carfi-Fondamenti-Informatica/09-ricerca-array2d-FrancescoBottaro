0b283d7eee3369ef56c3ed9633a8e1359aaa33a6
FrancescoBottaro <117718788+FrancescoBottaro@users.noreply.github.com>
2022-12-13 22:08:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+void ris (char a [10] [20] , char b [1] [20]) {
+
+    int contatore=0;
+    int c = 0;
+    int y = 0;
+
+    for (y = 0; y < 10; y++) {
+
+      while (a[y][c] == b[0][c]) {
+            c++;
+        }
+        if (c >= 20) {
+            cout<< y+1;
+            c = 0;
+            contatore++;
+        }
+    }
+    if (contatore<=0)
+        cout<< "non presente";
+}
